f9d7276e6b80d38316457151ceced54a1c4d7cd7
U1125794 <u1125794@umail.usq.edu.au>
2020-08-03 19:00:22 +0530

    CSC8426index.php

---
+++ index.php
@@ -0,0 +1,583 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RSBOOK</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+</head>
+<body>
+<section class="top">
+<nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">RS<span>BOOK</span></label>
+      <ul>
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="#books" >Books</a></li>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Shopping <i class="fas fa-cart-plus"></i></a></li>
+        <li><a href="#"><button class="register">Register</button></a></li>
+      </ul>
+</nav>
+
+</section>
+<section class="images">
+  <div class="search">
+    <div class="search1">
+      <form class="forms">
+        <div class="form-group">
+          <label for="selection"></label>
+          <select class="form-control" id="selection">
+            <option>Select Your Option</option>
+            <option>Authors</option>
+            <option>Books</option>
+            <option>Genre</option>
+            <option>ISBN</option>
+            <option>Publisher</option>
+          </select>
+        </div>
+        <div class="form-group1">
+          <input type="text" placeholder="search here" name="search">
+        </div>
+        <div class="form-group2">
+          <button class="button-search">Search</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</section>
+
+<section class="service" id="books">
+  <h2>New Releases</h2>
+  <div class="footerss">
+    <div class="footerss1 color-1">
+      <span class="icon"><img src="image/books.jpg"></span>
+      <h2>MIDNIGHT SUN</h2>
+      <div class="book">
+        <div class="books"><p><strong>Author:</strong> Stephenie Meyer</p></div>
+        <div class="books"><p><strong>Genre:</strong> Young Adult</p></div>
+      </div>
+      <div class="buy">
+        <div class="buys">
+          <button class="now">BUY NOW <i class="fas fa-cart-plus"></i></button>
+        </div>
+        <div class="buys">
+          <button class="comment">ADD COMMENT</button>
+        </div>
+      </div>
+    </div>
+    <div class="footerss1 color-2">
+      <span class="icon"><img src="image/books1.jpg"></span>
+      <h2>WOMEN AND LEADERSHIP</h2>
+      <div class="book">
+        <div class="books"><p><strong>Author:</strong> Julia Gillard</p></div>
+        <div class="books"><p><strong>Genre:</strong> Political Issues</p></div>
+      </div> 
+      <div class="buy">
+        <div class="buys">
+          <button class="now">BUY NOW <i class="fas fa-cart-plus"></i></button>
+        </div>
+        <div class="buys">
+          <button class="comment">ADD COMMENT</button>
+        </div>
+      </div>
+    </div>
+    <div class="footerss1 color-3">
+      <span class="icon"><img src="image/books2.jpg"></span>
+      <h2>THE ICKABOG</h2>
+      <div class="book">
+        <div class="books"><p><strong>Author:</strong> J.K. Rowling</p></div>
+        <div class="books"><p><strong>Genre:</strong>  Children's Fiction</p></div>
+      </div>
+      <div class="buy">
+        <div class="buys">
+          <button class="now">BUY NOW <i class="fas fa-cart-plus"></i></button>
+        </div>
+        <div class="buys">
+          <button class="comment">ADD COMMENT</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footerss">
+  <div class="footerss1 color-4">
+      <span class="icon"><img src="image/books3.jpg"></span>
+      <h2>THE ORDER</h2>
+      <div class="book">
+        <div class="books"><p><strong>Author:</strong> Daniel Silva</p></div>
+        <div class="books"><p><strong>Genre:</strong> Mystery & Thriller</p></div>
+      </div>
+      <div class="buy">
+        <div class="buys">
+          <button class="now">BUY NOW <i class="fas fa-cart-plus"></i></button>
+        </div>
+        <div class="buys">
+          <button class="comment">ADD COMMENT</button>
+        </div>
+      </div>
+    </div>
+    <div class="footerss1 color-5">
+      <span class="icon"><img src="image/books4.jpg"></span>
+      <h2>THE SILENT WIFE</h2>
+      <div class="book">
+        <div class="books"><p><strong>Author:</strong> Karin Slaughter</p></div>
+        <div class="books"><p><strong>Genre:</strong>  Mystery & Thriller</p></div>
+      </div>
+      <div class="buy">
+        <div class="buys">
+          <button class="now">BUY NOW <i class="fas fa-cart-plus"></i></button>
+        </div>
+        <div class="buys">
+          <button class="comment">ADD COMMENT</button>
+        </div>
+      </div>
+    </div>
+    <div class="footerss1 color-6">
+      <span class="icon"><img src="image/books5.jpg"></span>
+      <h2>THE YIELD</h2>
+      <div class="book">
+        <div class="books"><p><strong>Author:</strong> Tara June Winch</p></div>
+        <div class="books"><p><strong>Genre:</strong> Sociology</p></div>
+      </div>
+      <div class="buy">
+        <div class="buys">
+          <button class="now">BUY NOW <i class="fas fa-cart-plus"></i></button>
+        </div>
+        <div class="buys">
+          <button class="comment">ADD COMMENT</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="first" id="blog">
+    <h2>Our News & Blogs</h2>
+      <div class="seconds">
+        <div class="grid-contain">
+          <div class="grid-item">
+            <img src="image/blog.jpg" alt="blogs">
+            <h4>Wordpress for a beginner</h4>
+            <p>Suspendisse potenti. Donec congue vel justo eget malesuada. In arcu justo, sagittis consequat pulvinar quis, pretium quis massa. Vestibulum nec nibh eu nisi rutrum iaculis.</p>
+          </div>  
+          <div class="grid-item">
+            <img src="image/blog1.jpg" alt="blogs">
+            <h4>Make Website From Scratchk</h4>
+            <p>Suspendisse potenti. Donec congue vel justo eget malesuada. In arcu justo, sagittis consequat pulvinar quis, pretium quis massa. Vestibulum nec nibh eu nisi rutrum iaculis.</p>
+          </div>  
+          <div class="grid-item">
+            <img src="image/blog2.jpg" alt="blogs">
+            <h4>Renovating National Gallery</h4>
+            <p>Suspendisse potenti. Donec congue vel justo eget malesuada. In arcu justo, sagittis consequat pulvinar quis, pretium quis massa. Vestibulum nec nibh eu nisi rutrum iaculis.</p>
+          </div>
+          </div>
+          <div class="grid-contain">
+          <div class="grid-item">
+            <img src="image/blog3.webp" alt="blogs">
+            <h4>Why Lead Generation is Key for Business Growth</h4>
+            <p>Suspendisse potenti. Donec congue vel justo eget malesuada. In arcu justo, sagittis consequat pulvinar quis, pretium quis massa. Vestibulum nec nibh eu nisi rutrum iaculis.</p>
+          </div>
+          <div class="grid-item">
+            <img src="image/blog4.webp" alt="blogs">
+            <h4>10 Best Remote Work Tips for Design Teams in 2020</h4>
+            <p>Suspendisse potenti. Donec congue vel justo eget malesuada. In arcu justo, sagittis consequat pulvinar quis, pretium quis massa. Vestibulum nec nibh eu nisi rutrum iaculis.</p>
+          </div>  
+          <div class="grid-item">
+            <img src="image/blog5.jpg" alt="blogs">
+            <h4>40 Best Modern Website Design Examples &Templates in 2020</h4>
+            <p>Suspendisse potenti. Donec congue vel justo eget malesuada. In arcu justo, sagittis consequat pulvinar quis, pretium quis massa. Vestibulum nec nibh eu nisi rutrum iaculis.</p>
+          </div>
+          </div>   
+        </div>
+      </div>
+    </section>
+
+
+
+<section class="footer">
+  <div class="footer1">
+    <p>Copyright @ 2020. All Rights Reserved</p>
+  </div>
+</section>
+
+
+<style type="text/css">
+  
+
+*{
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+  list-style: none;
+  box-sizing: border-box;
+}
+body{
+  font-family: Arial,verdana;
+}
+nav{
+  background: #252527;
+  height: 65px;
+  width: 100%;
+  position: fixed;
+}
+label.logo{
+  color: #fff;
+  font-size: 30px !important;
+  line-height: 65px;
+  padding: 0 100px;
+  font-weight: bold;
+}
+.logo span{
+  color: red;
+}
+nav ul{
+  float: right;
+  margin-right: 40px;
+}
+nav ul li{
+  display: inline-block;
+  line-height: 65px;
+  margin: 0 5px;
+}
+nav ul li a{
+  color: #fff;
+  font-size: 15px;
+  padding: 7px 13px;
+  border-radius: 3px;
+  text-transform: uppercase;
+}
+.checkbtn{
+  font-size: 30px;
+  color: white;
+  float: right;
+  line-height: 65px;
+  margin-right: 40px;
+  cursor: pointer;
+  display: none;
+}
+#check{
+  display: none;
+}
+@media (max-width: 952px){
+  nav ul li a{
+    font-size: 16px;
+  }
+}
+@media (max-width: 858px){
+  .checkbtn{
+    display: block;
+  }
+  ul{
+    position: absolute;
+    width: 100%;
+    height: 125vh;
+    background: yellowgreen;
+    top: 80px;
+    left: -100%;
+    text-align: center;
+    transition: all .5s;
+    z-index: 999;
+  }
+  nav ul li{
+    display: block;
+    margin: 30px 0;
+    line-height: 30px;
+  }
+  nav ul li a{
+    font-size: 17px;
+  }
+  a:hover,a.active{
+    background: none;
+    color: #0082e6;
+  }
+  #check:checked ~ ul{
+    left: 0;
+  }
+  .service .footerss{
+    display: flex;
+    flex-direction: column;
+  }
+  .seconds .grid-contain{
+    display: flex;
+    flex-direction: column;
+  }
+  .grid-contain .grid-item{
+    width: 100%;
+  }
+  .service .footerss .footerss1{
+    width: 100%;
+  }
+  .images .search{
+    padding-top: 16%;
+  }
+  }
+
+  .images{
+    background: url('image/image.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 550px;
+  }
+  .search{
+    display: block;
+    max-width: 1080px;
+    margin: auto;
+    padding-top: 16%;
+  }
+  .forms{
+    display: flex;
+    flex-direction: row;
+  }
+  .form-group select{
+    min-height: 48px;
+    font-size: 16px;
+    width: 100%;
+    border-radius: 6px 0 0 6px;
+    border: none;
+    padding: 0 10px;
+  }
+  .form-group1 input{
+    overflow: hidden;
+    text-overflow: ellipsis;
+    width: 100%;
+    height: 100%;
+    border: none;
+    border-left: 1px solid;
+    padding-left: 20px;
+  }
+  .form-group2 button{
+    display: block;
+    width: auto;
+    padding: 0 6px;
+    min-height: 48px;
+    font-size: 0.875;
+    text-transform: uppercase;
+    border-radius: 0 8px 8px 0;
+    border:none;
+  }
+  .forms .form-group{
+    width: 15%;
+  }
+  .forms .form-group1{
+    width: 75%;
+  }
+  .forms .form-group2{
+    width: 10%;
+  }
+  .search1{
+   
+    background: rgba(0, 0, 0, 0.5);
+    padding: 16px;
+  }
+  .register{
+    position: relative;
+    background-color: #f39c12;
+    border: none;
+    padding: 10px;
+    width: 100px;
+    font-size: 17px;
+    text-align: center;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    text-decoration: none;
+    overflow: hidden;
+  }
+  .register:hover{
+   background:#fff;
+   box-shadow:0px 2px 10px 5px #97B1BF;
+   color:#000;
+  }
+
+
+  .service{
+  padding: 60px;
+  background-color: #38DCC8;
+}
+.footerss1{
+  margin: 0 auto;
+  text-align: center;
+  padding: 50px;
+  text-transformation: uppercase;
+  font-family: 'arial', sans-serif;
+  font-weight: bold;
+  margin-top: 45px;
+ color:white;
+  padding:20px;
+  display: block;
+  background-image: linear-gradient(to left,
+    transparent,
+    transparent 50%,
+    #00c6ff 50%,
+    #00c6ff);
+  background-position: 100% 0;
+    background-size: 200% 100%;
+    transition: all .25s ease-in;
+    
+}
+.footerss1{
+  box-shadow: 0 0 130px 81px rgba(0,0,0,.17);
+}
+
+.footerss1.color-1 {
+    border-bottom: 3px solid #2c98f0;
+}
+.footerss1.color-2 {
+    border-bottom: 3px solid #ec5453;
+}
+.footerss1.color-3 {
+    border-bottom: 3px solid #f9bf3f;
+}
+.footerss1.color-4 {
+    border-bottom: 3px solid #a84cb8;
+}
+.footerss1.color-5 {
+    border-bottom: 3px solid #2fa499;
+}
+.footerss1.color-6 {
+    border-bottom: 3px solid #4054b2;
+}
+.footerss{
+  display: flex;
+  flex-direction: row;
+}
+.footerss1{
+  text-align: center;
+  background-color: #fff;
+}
+.footerss .footerss1{
+  border: 1px solid #38DCC8;
+    padding: 45px 20px;
+    margin-bottom: 30px;
+    margin-left: 15px;
+    margin-right: 15px;
+    width: 31%;
+}
+.footerss .footerss1 p{
+  font-size: 1.1rem;
+  color: #686868;
+  padding: 0px 35px;
+  line-height: 28px;
+  font-weight: 200;
+}
+.footerss .footerss1 h2{
+  color: #000;
+    margin-top: 22px;
+    margin-bottom: 11px;
+    font-weight: 400;
+    font-size: 1.2rem;
+}
+.service h2{
+  font-size: 3rem;
+  text-align: center;
+}
+
+
+.seconds{
+   max-width: 1133px;
+    margin: auto;
+    padding-bottom: 60px;
+    padding-top: 50px;
+}
+.grid-contain {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding: 0 30px;
+}
+
+.grid-item {
+  margin: 3% 1% 0.5% 1%;
+  width: 31%;
+}
+.first{
+  background: #EAF1FB;
+}
+
+.grid-item h4{
+  text-align: left;
+  font-size: 30px;
+  padding-top: 10px;
+  font-weight: normal;
+  padding: 8px 8px;
+  font-weight: 600;
+}
+.grid-item h4:hover{
+  color: #ff006c;
+}
+.grid-item p{
+  padding: 10px 5px;
+  font-size: 16px;
+  color: #71767b;
+}
+.grid-item h4 .hov{
+  color: #fff;
+}
+.grid-item img{
+  width: 100%;
+  height: auto;
+}
+.grid-item:hover{
+  box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.16);
+    -o-box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.16);
+    -moz-box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.16);
+    -webkit-box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.16);
+}
+.first h2{
+  font-size: 2.5rem;
+  text-align: center;
+  padding-top: 30px;
+}
+.footer{
+  background: #252527;
+  padding: 20px 0;
+}
+.footer1 p{
+  font-size: 18px;
+  color: #fff;
+  text-align: center;
+}
+
+/*.fas {
+    font-size: 1rem;
+    color: #fff;
+}*/
+.book{
+  display: flex;
+  flex-direction: row;
+}
+.book .books{
+  color: #686868;
+  width: 50%;
+}
+.book .books p{
+  font-size: 0.8rem;
+  padding: 0 5px;
+}
+.buy{
+  display: flex;
+  flex-direction: row;
+  padding: 15px 0;
+}
+.buy .buys{
+  width: 50%
+}
+.buys .now{
+  padding: 8px 10px;
+  border:none;
+  background: yellowgreen;
+  color: #fff;
+}
+.buys .comment{
+   padding: 8px 10px;
+  border:none;
+  background: yellow;
+  color: #000;
+}
+</style>
